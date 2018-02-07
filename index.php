1a5a4c45b6bfcdc272fc07d81b156131bc9d9bc5
Gildardo Orozco <gorozco@csumb.edu>
2018-02-07 20:39:46 +0000

    HomePage work

--- index.php
+++ index.php
@@ -3,8 +3,41 @@
     <head>
         <meta charset="utf-8"/>
         <title>Welcome Page!</title>
+                
+        <meta name="viewport" content="width=device-width">
+        <link href="css/styles.css" rel="stylesheet" type="text/css" />
+        <link href="https://fonts.googleapis.com/css?family=Libre+Barcode+39+Text" rel="stylesheet">
+        
+        
     </head>
     <body>
-        <a href="/labs/lab1/index.html" >Lab 1!</a>
+        <header>
+            <h1> Gildardo Orozco</h1>
+        </header>
+        
+        <nav>    
+            <hr width="50%" id="line"/>
+            <ol>
+                <a href="/labs/lab1/index.html" >Lab 1</a>
+                <a href="/assignments/hw1/index.php" >HW 1</a>
+                
+            </ol>
+            
+        </nav>
+        <main>
+            <figure id="me">
+                <img width="60%" src="img/csumbChurch.jpg" alt="Picture of old church at CSUMB" />
+            </figure>
+        
+        </main>
+        <br/><br/>
+        
+        <footer>
+            <hr width="50%"/>
+            2017&copy; Orozco <br/>
+            <strong>Disclaimer:</strong> The information in this webpage is of my class assignments. <br/>
+            Don't copy.  <br/>
+         </footer>
     </body>
+    
 </html>
