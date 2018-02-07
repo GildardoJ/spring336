6d18b0f3108d8647194f5e777f77a4416164e1f1
Gildardo Orozco <gorozco@csumb.edu>
2018-02-07 21:19:39 +0000

    hw1

---
+++ assignments/hw1/index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html>
+  
+<head>
+  <title>Programming languages</title>
+  <meta charset="utf-8"> 
+  <meta name="viewport" content="width=device-width">
+  <link href="/gorozco/HomeWork/hw1/css/styles.css" rel="stylesheet" type="text/css" />
+  
+  <link href="https://fonts.googleapis.com/css?family=Libre+Barcode+39+Text" rel="stylesheet">
+  
+</head>
+
+<body>
+  
+  <header>
+    <h1> Programming Languages</h1>
+  </header> 
+  
+
+  <nav>
+    <hr width="50%" id="line"/>
+     <ol>
+      <a href="index.html">Home</a>
+      <a href="css.html">CSS</a>
+      <a href="php.html">PHP</a>
+      <a href="python.html">PYTHON</a>
+     </ol>
+  </nav>
+  
+  <br /><br />
+  
+  <main>
+    
+    <figure id="me">
+      <img src="img/codelaptop.jpg" alt="Picture laptop with code on screen" />
+    </figure>
+    
+      <div id="welcomeText" >
+        Languages 
+        <p>There are many different types of programming languages</p>
+        <p>Here we will describe a couple of them.</p>
+        <p>May this be usefull for you. </p>
+        
+        <br /><br />
+        
+        <em>"With no talent and extraordinary 
+        <strong> perserverance</strong>, you can be president"</em><br />
+        - No One
+        
+      </div>
+      
+  </main>
+  
+  <footer>
+    <hr width="50%"/>
+    2017&copy; Orozco <br/>
+    <strong>Disclaimer:</strong> The information in this webpage is fictitous. <br/>
+    It is used for academic purposes only. <br/>
+    <img src="img/icon-2488093_640.png" alt="Picture of books" />
+  </footer>
+  
+  
+</body>
+</html>
