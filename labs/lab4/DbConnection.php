444a1ffc262398ebe725760b1bba8544eed18fa5
Gildardo Orozco <gorozco@csumb.edu>
2018-03-13 08:18:18 +0000

    lab 4 Heroku

--- labs/lab4/DbConnection.php
+++ labs/lab4/DbConnection.php
@@ -9,10 +9,10 @@ function getDataBaseconnection($opt){
     
     if  (strpos($_SERVER['HTTP_HOST'], 'herokuapp') !== false) {
         $url = parse_url(getenv("JAWSDB_MARIA_URL"));
-        $host = "gmgcjwawatv599gq.cbetxkdyhwsb.us-east-1.rds.amazonaws.com	";
+        $host = $url["host"];
         $dbname = substr($url["path"], 1);
-        $username = $url["cfwgru3ia5mrhhgk"];
-        $password = $url["jvizwj9otiayhs9q"];
+        $username = $url["user"];
+        $password = $url["pass"];
     } 
     
     //mysql://b80082c70b0018:56c605d4@us-cdbr-iron-east-05.cleardb.net/heroku_681bddbab572813?reconnect=true
