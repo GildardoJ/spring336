fb56150d13ac75863c9a6502d5a763e47d8e2699
Gildardo Orozco <gorozco@csumb.edu>
2018-03-13 08:13:42 +0000

    lab 4

--- labs/lab4/DbConnection.php
+++ labs/lab4/DbConnection.php
@@ -9,13 +9,13 @@ function getDataBaseconnection($opt){
     
     if  (strpos($_SERVER['HTTP_HOST'], 'herokuapp') !== false) {
         $url = parse_url(getenv("JAWSDB_MARIA_URL"));
-        $host = $url["host"];
+        $host = "gmgcjwawatv599gq.cbetxkdyhwsb.us-east-1.rds.amazonaws.com	";
         $dbname = substr($url["path"], 1);
-        $username = $url["user"];
-        $password = $url["pass"];
+        $username = $url["cfwgru3ia5mrhhgk"];
+        $password = $url["jvizwj9otiayhs9q"];
     } 
     
-    mysql://b80082c70b0018:56c605d4@us-cdbr-iron-east-05.cleardb.net/heroku_681bddbab572813?reconnect=true
+    //mysql://b80082c70b0018:56c605d4@us-cdbr-iron-east-05.cleardb.net/heroku_681bddbab572813?reconnect=true
     $dbConn = new PDO("mysql:host=$host;dbname=$dbname",$username,$password);
     
     $dbConn -> setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
